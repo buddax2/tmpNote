be55ce5c4390d7508dc91c90cc3e2d8c83ad8629
Oleksandr Yakubchyk <oleksandr.yakubchyk@weather.com>
2017-09-23 00:45:18 +0300

    Initial Commit

---
+++ tmpNote/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  tmpNote
+//
+//  Created by BUDDAx2 on 9/23/17.
+//  Copyright © 2017 BUDDAx2. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ tmpNote/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tmpNote
+//
+//  Created by BUDDAx2 on 9/23/17.
+//  Copyright © 2017 BUDDAx2. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
