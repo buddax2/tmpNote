92a43c35a5404ca1b6d906bb6951c8de0b4cf7f7
Oleksandr Yakubchyk <Oleksandr.Yakubchyk@ua.ibm.com>
2020-02-15 07:11:58 +0200

    removed redundant code

--- tmpNote/TmpNoteViewController.swift
+++ tmpNote/TmpNoteViewController.swift
@@ -247,8 +247,6 @@ class TmpNoteViewController: NSViewController, NSTextViewDelegate {
         textView.font = font
     }
     
-    var directoryObserver: DirectoryObserver?
-    
     func loadPreviousText() {
         loadSubstitutions()
         
@@ -677,29 +675,6 @@ extension TmpNoteViewController {
         migrateText()
         migrateSketch()
     }
-}
-
-class DirectoryObserver {
-
-    private let fileDescriptor: CInt
-    private let source: DispatchSourceProtocol
-
-    deinit {
-
-      self.source.cancel()
-      close(fileDescriptor)
-    }
-
-    init(URL: URL, block: @escaping ()->Void) {
-
-      self.fileDescriptor = open(URL.path, O_EVTONLY)
-        self.source = DispatchSource.makeFileSystemObjectSource(fileDescriptor: self.fileDescriptor, eventMask: .all, queue: DispatchQueue.global())
-      self.source.setEventHandler {
-          block()
-      }
-      self.source.resume()
-  }
-
 }
 
 class CustomRadioButton: NSButton {
